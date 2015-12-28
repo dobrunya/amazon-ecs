31c710a90abcbbc65d7cd86c971be05876831f71
dobrunya <dobrunya777@gmail.com>
2015-12-28 22:12:35 +0200

    Fixes for mx

--- lib/amazon/ecs.rb
+++ lib/amazon/ecs.rb
@@ -45,8 +45,8 @@ module Amazon
         :cn => 'http://webservices.amazon.cn/onca/xml',
         :es => 'http://webservices.amazon.es/onca/xml',
         :in => 'http://webservices.amazon.in/onca/xml',
-        :br => 'http://webservices.amazon.com.br/onca/xml
-        :mx => 'http://webservices.amazon.com.mx/onca/xml''
+        :br => 'http://webservices.amazon.com.br/onca/xml',
+        :mx => 'http://webservices.amazon.com.mx/onca/xml'
     }
 
     OPENSSL_DIGEST_SUPPORT = OpenSSL::Digest.constants.include?( 'SHA256' ) ||
